5a56825b324b297c10fa0ead61022d162c68625f
markostupar98 <markostuparets@gmail.com>
2024-06-10 22:06:21 +0200

    adding cv

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -15,5 +15,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "said7388",
   resume:
-    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+    "https://drive.google.com/file/d/1RlIk5Dixp1qMeYWS8b8NIZTAQ773MyGs/view?usp=sharing",
 };
